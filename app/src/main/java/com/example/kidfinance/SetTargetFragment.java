9badeadd8e408f094b0d5e74d628d2aa7c7524a9
mekptang <36660688+mekptang@users.noreply.github.com>
2019-04-15 02:09:03 +0800

    target item list

--- app/src/main/java/com/example/kidfinance/SetTargetFragment.java
+++ app/src/main/java/com/example/kidfinance/SetTargetFragment.java
@@ -1,17 +1,95 @@
 package com.example.kidfinance;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SetTargetFragment extends Fragment {
 
-    @Nullable
+    private ArrayList<String> data = new ArrayList<String>();
+
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle saveInstanceState){
-        return inflater.inflate(R.layout.fragment_target, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle saveInstanceState){
+        View view = inflater.inflate(R.layout.fragment_target, container, false);
+        ListView lv = (ListView) view.findViewById(R.id.target_item_list);
+        generateListContent();
+        lv.setAdapter(new MyListAdapter(getContext(), R.layout.item_list_set_target, data));
+        return view;
+    }
+
+    private void generateListContent() {
+        for(int i = 0; i < 10; i++){
+            data.add("this is row number " + i);
+        }
+    }
+
+    private class MyListAdapter extends ArrayAdapter<String>{
+        private int layout;
+        private MyListAdapter(Context context, int resource, List<String> objects) {
+            super(context, resource, objects);
+            layout = resource;
+        }
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent){
+            ViewHolder mainViewHolder = null;
+            if(convertView == null){
+                LayoutInflater inflater = LayoutInflater.from(getContext());
+                convertView = inflater.inflate(layout, parent, false);
+                ViewHolder viewHolder = new ViewHolder();
+                viewHolder.item_logo = (ImageView) convertView.findViewById(R.id.item_logo);
+                viewHolder.item_count = (TextView) convertView.findViewById(R.id.item_count);
+                viewHolder.plus_item = (ImageButton) convertView.findViewById(R.id.plus_item);
+                viewHolder.minus_item = (ImageButton) convertView.findViewById(R.id.minus_item);
+                viewHolder.item_cancel = (ImageButton) convertView.findViewById(R.id.item_cancel);
+
+                viewHolder.plus_item.setOnClickListener(new View.OnClickListener(){
+                    @Override
+                    public void onClick(View v){
+                        Toast.makeText(getContext(), "+", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                viewHolder.minus_item.setOnClickListener(new View.OnClickListener(){
+                    @Override
+                    public void onClick(View v){
+                        Toast.makeText(getContext(), "-", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                viewHolder.item_cancel.setOnClickListener(new View.OnClickListener(){
+                    @Override
+                    public void onClick(View v){
+                        Toast.makeText(getContext(), "x", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                convertView.setTag(viewHolder);
+
+            }else{
+
+                mainViewHolder = (ViewHolder) convertView.getTag();
+
+            }
+
+            return convertView;
+        }
+    }
+    public class ViewHolder{
+        ImageView item_logo;
+        TextView item_count;
+        ImageButton plus_item;
+        ImageButton minus_item;
+        ImageButton item_cancel;
     }
 }
