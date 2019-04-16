a75fe8287efad6912e66de3e4a9b758bb4c127bc
nvwong <27146493+nvwong@users.noreply.github.com>
2019-04-16 16:43:25 +0800

    set awards list, button click shows what have been chosen.

--- app/src/main/java/com/example/kidfinance/AwardFragment.java
+++ app/src/main/java/com/example/kidfinance/AwardFragment.java
@@ -10,8 +10,10 @@ import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,13 +23,19 @@ public class AwardFragment extends Fragment {
     ViewPager viewPager;
     AwardAdapter adapter;
     List<AwardModel> awards;
+    Button setAwardBtn;
     int award_index = 0;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle saveInstanceState) {
         awards = new ArrayList<>();
-
+        awards.add(new AwardModel(R.drawable.award_hamburger, "Hamburger", "Have a nice burger for a treat!"));
+        awards.add(new AwardModel(R.drawable.award_comic, "Comic Books", "Buy your favourite comic series."));
+        awards.add(new AwardModel(R.drawable.award_disney, "Disney Trip", "Go to Disneyland for a nice day."));
+        awards.add(new AwardModel(R.drawable.award_juice, "Juice", "Have some juice to refresh!"));
+        awards.add(new AwardModel(R.drawable.award_milktea, "Bubble Milk Tea", "How nice to have a cup of bubble tea after a long day!"));
+        awards.add(new AwardModel(R.drawable.award_movie, "Watch a Movie", "How about watching a new movie?"));
         return inflater.inflate(R.layout.fragment_award, container, false);
     }
 
@@ -39,6 +47,31 @@ public class AwardFragment extends Fragment {
         adapter = new AwardAdapter(awards, getActivity());
         viewPager.setAdapter(adapter);
         viewPager.setPadding(130, 0, 130, 0);
+
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int i, float v, int i1) {
+
+            }
+
+            @Override
+            public void onPageSelected(int i) {
+                award_index = i;
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int i) {
+
+            }
+        });
+
+        setAwardBtn = view.findViewById(R.id.setAwardBtn);
+        setAwardBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), awards.get(award_index).getName() + " has been chosen as award.", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
 
