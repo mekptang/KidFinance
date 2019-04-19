0a7e3ed365c1100527d32a56ef33d81d5718a6b0
mekptang <36660688+mekptang@users.noreply.github.com>
2019-04-19 16:52:59 +0800

    ProgressFragment modified targetValue

--- app/src/main/java/com/example/kidfinance/ProgessFragment.java
+++ app/src/main/java/com/example/kidfinance/ProgessFragment.java
@@ -9,7 +9,12 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -27,8 +32,16 @@ public class ProgessFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle saveInstanceState) {
-        targetValue = 6000;
+        String tempVal = loadTextFile("kf_target_money_config.txt");
+        if(tempVal != ""){
+            targetValue = Integer.parseInt(tempVal);
+        }else{
+            targetValue = 0;
+        }
+
         currentSaving = 2500;
+
+
 
         return inflater.inflate(R.layout.fragment_progress, container, false);
     }
@@ -53,5 +66,29 @@ public class ProgessFragment extends Fragment {
         message.setText("You reached " + percent + "% of your target! Add oil!");
 
         super.onViewCreated(view, savedInstanceState);
+    }
+
+    public String loadTextFile(String fileName)
+    {
+        String text = "";
+        try {
+            FileInputStream inStream = getContext().openFileInput(fileName);
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+            byte[] buffer = new byte[1024];
+            int length = -1;
+            while((length = inStream.read(buffer))!=-1) {
+                stream.write(buffer,0,length);
+            }
+            stream.close();
+            inStream.close();
+            text = stream.toString();
+            Toast.makeText(getContext(),"Loaded",Toast.LENGTH_LONG).show();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        catch (IOException e){
+            return e.toString();
+        }
+        return text;
     }
 }
